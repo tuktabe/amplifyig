7ba94340db1dd18aa31b7c48d1b041da057eec80
tuktabe <tukta.belding@outlook.com>
2023-11-13 21:00:14 -0800

    try sign out

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { PostCollection } from "./ui-components";
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
 
 function App() {
   return (
@@ -9,6 +10,14 @@ function App() {
     <PostCollection />
   </div>
   );
+}
+
+async function signOut() {
+  try {
+    await Auth.signOut();
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
 }
       
 
