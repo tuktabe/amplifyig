a01104487a3253e7f604e870e0a30de5c48c8c93
tuktabe <tukta.belding@outlook.com>
2023-11-14 18:51:00 -0800

    feedpagecollection test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { PostCollection } from "./ui-components";
+import { FeedpageCollection, PostCollection } from "./ui-components";
 import { withAuthenticator, Button } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify';
 import React, { useState, useEffect } from "react";
@@ -8,7 +8,7 @@ import React, { useState, useEffect } from "react";
 function App({ signOut }) {
   return (
   <div className="App">
-    <UploadpageCollection />
+    <FeedpageCollection />
     <Button onClick={signOut}>Sign Out</Button>
   </div> 
   );
