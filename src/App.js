ab7aad8e3a8a7da40e91d78e246aeadcaa7544fc
tuktabe <tukta.belding@outlook.com>
2023-11-20 17:06:33 -0800

    vdo background

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import { FeedpageCollection, ModelcatCollection, NewfeedCollection, PostCollection, ProfileCollection } from "./ui-components";
 import { withAuthenticator, Button } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify';
-import React, { useState, useEffect } from "react";
+import Main from './Main';
 
 function App({ signOut }) {
 return (
 <div className="App">
+
 <ModelcatCollection />  
 <Button onClick={signOut}>Sign Out</Button>
 </div> 
 );
 }
+
+
 
 
 export default withAuthenticator(App)
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+//import React from 'react';
+//import videoBg from './images/videoBg.mp4';
+
+
+//function Main() {
+    //return (
+        //<div className='main'>
+            //<video src={videoBg} autoPlay loop muted />
+        //</div>
+   //);
+//} 
+
+//export default Main;
