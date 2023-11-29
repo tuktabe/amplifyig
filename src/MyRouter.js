a7f9c016f76ac4063adbb65f7c1ab7d7d1e28d4e
Tukta Belding <tukta.belding@outlook.com>
2023-11-28 19:49:33 -0800

    Testing again

--- src/MyRouter.js
+++ src/MyRouter.js
@@ -14,7 +14,7 @@ export default function Root() {
           <Route exact path="/" component={Discover16Collection} />
           <Route exact path="/Discover16Collection" component={Discover16Collection} />
           <Route path="/GalleryCollection" component={GalleryCollection} />
-          <Route path="/GalleryCollection/" component={GalleryCollection} />
+          <Route path="/GalleryCollection/" component={GalleryCollection} /> 
           <Route exact path="/ProfileScreenCollection" component={ProfileScreenCollection} />
           <Route exact path="/Myuploadpage" component={CreateUser} />
           <Route exact path="/mypageupload" component={mypageupload} />
